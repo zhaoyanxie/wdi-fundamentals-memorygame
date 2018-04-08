1bae25218bcb5326624591a343e9bb79961f3f24
zhaoyanXie <u076745e@alumni.nus.edu.sg>
2018-04-08 13:39:02 +0800

    Unit 8 - card matching logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king']
+var cardsInPlay = []
+var cardOne
+var cardTwo
 
-console.log("User flipped " + cardOne);
+cardOne = cards[0]
+cardsInPlay.push(cardOne)
+
+cardTwo = cards[2]
+cardsInPlay.push(cardTwo)
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!')
+  } else {
+    alert('Sorry, try again')
+  }
+}
+
+console.log(`User flipped ${cardsInPlay}`)
